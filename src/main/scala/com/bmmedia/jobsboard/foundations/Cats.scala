bbef54aef4031270faef9cff368509cccc14badc
Bert Meeuws <bert.meeuws@inthepocket.com>
2023-05-23 20:43:42 +0200

    feat: cats crash course

--- src/main/scala/com/bmmedia/jobsboard/foundations/Cats.scala
+++ src/main/scala/com/bmmedia/jobsboard/foundations/Cats.scala
@@ -1,5 +1,7 @@
 package com.bmmedia.foundations
 
+import scala.math.Integral.Implicits.infixIntegralOps
+
 object Cats {
 
   trait MyFunctor[F[_]] {
@@ -36,17 +38,67 @@ object Cats {
     val aSimpleList_v2 = 42.pure[List]
 
     // FlatMap
-    trait MyFlatMap[F[]] extends Functor[F] {
-      def flatMap[A,B](initialValue: F[A])(f: A => F[B]): F[B]
+    trait MyFlatMap[F[_]] extends Functor[F] {
+      def flatMap[A, B](initialValue: F[A])(f: A => F[B]): F[B]
     }
 
     import cats.FlatMap
 
-    val flatMapList = FlatMap[List]
+    val flatMapList    = FlatMap[List]
+    val flatMappedList = flatMapList.flatMap(List(1, 2, 3))(x => List(x, x + 1))
 
+    import cats.syntax.flatMap.*
 
+    def crossProduct[F[_]: FlatMap, A, B](containerA: F[A], containerB: F[B]): F[(A, B)] = {
+      containerA.flatMap(a => containerB.map(b => (a, b)))
+    }
 
+    def crossProduct_v2[F[_]: FlatMap, A, B](containerA: F[A], containerB: F[B]): F[(A, B)] = {
+      for {
+        a <- containerA
+        b <- containerB
+      } yield (a, b)
+    }
 
+    // Monad
+    trait MyMonad[F[_]] extends MyApplicative[F] with MyFlatMap[F] {
+      override def map[A, B](initialValue: F[A])(f: A => B): F[B] =
+        flatMap(initialValue)(a => pure(f(a)))
+    }
 
-  def main(args: Array[String]): Unit = {}
+    import cats.Monad
+
+    val monadList    = Monad[List]
+    val extendedList = monadList.map(List(1, 2, 3))(_ + 2)
+
+    def crossProduct_v3[F[_]: Monad, A, B](containerA: F[A], containerB: F[B]): F[(A, B)] = {
+      for {
+        a <- containerA
+        b <- containerB
+      } yield (a, b)
+    }
+
+    trait ApplicativeError[F[_], E] extends Applicative[F] {
+      def raiseError[A](error: E): F[A]
+    }
+
+    import cats.ApplicativeError
+
+    type ErrorOr[A] = Either[String, A]
+    val applicativeEither          = ApplicativeError[ErrorOr, String]
+    val desiredValue: ErrorOr[Int] = applicativeEither.pure(42)
+    val anError: ErrorOr[Int]      = applicativeEither.raiseError("Something went wrong")
+
+    import cats.syntax.applicativeError.*
+    val failedValue_v2: ErrorOr[Int] = "Something went wrong".raiseError[ErrorOr, Int]
+
+    trait MyMonadError[F[_], E] extends Monad[F] with ApplicativeError[F, E]
+
+    import cats.MonadError
+
+    val monadErrorEither = MonadError[ErrorOr, String]
+
+  def main(args: Array[String]): Unit = {
+    Unit
+  }
 }
