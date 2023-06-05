b91b129bf493cbf2691ba58553a4ece005a5e8a3
Bert Meeuws <bert.meeuws@outlook.com>
2023-06-05 18:59:58 +0200

    feat: added our first working test

--- src/test/scala/com/bmmedia/http/routes/JobRoutesSpec.scala
+++ src/test/scala/com/bmmedia/http/routes/JobRoutesSpec.scala
@@ -1,5 +1,7 @@
 package com.bmmedia.http.routes
 
+import io.circe.generic.auto.*
+import org.http4s.circe.CirceEntityCodec.*
 import com.bmmedia.fixtures.JobFixture
 import org.scalatest.freespec.AsyncFreeSpec
 import org.http4s.dsl.Http4sDsl
@@ -14,6 +16,8 @@ import com.bmmedia.jobsboard.http.routes.*
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 import org.http4s.HttpRoutes
+import org.http4s.Request
+import org.http4s.implicits.*
 
 class JobRoutesSpec
     extends AsyncFreeSpec
@@ -32,9 +36,24 @@ class JobRoutesSpec
     override def delete(id: UUID): IO[Int] = IO.pure(1)
 
     override def findAll(): IO[List[Job]] = IO.pure(List(AwesomeJob))
-
-    given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
-
-    val jobRoutes: HttpRoutes[IO] = JobRoutes[IO](jobs).routes
   }
+
+  given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
+
+  val jobRoutes: HttpRoutes[IO] = JobRoutes[IO](jobs).routes
+
+  "JobRoutes" - {
+    "should return a job with a given id" in {
+      for {
+        response <- jobRoutes.orNotFound.run(
+          Request(GET, uri"/jobs/843df718-ec6e-4d49-9289-f799c0f40064")
+        )
+        retrieved <- response.as[Job]
+      } yield {
+        response.status shouldBe Ok
+        retrieved shouldBe AwesomeJob
+      }
+    }
+  }
+
 }
