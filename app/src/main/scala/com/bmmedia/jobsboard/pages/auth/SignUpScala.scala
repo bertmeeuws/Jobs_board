262d095a47d07a2c13d888060b15c1df92c71843
Bert Meeuws <bert.meeuws@outlook.com>
2023-07-06 20:54:15 +0200

    wip: working on sign up form

--- app/src/main/scala/com/bmmedia/jobsboard/pages/auth/SignUpScala.scala
+++ app/src/main/scala/com/bmmedia/jobsboard/pages/auth/SignUpScala.scala
@@ -5,9 +5,16 @@ import tyrian.Html.*
 import cats.effect.IO
 import com.bmmedia.jobsboard.pages.Page.Msg
 import com.bmmedia.jobsboard.pages.Page
+import com.bmmedia.jobsboard.pages.auth.SignUpPage.NoOp
 
-final case class SignUpPage() extends Page:
-
+final case class SignUpPage(
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    confirmPassword: String,
+    company: String
+) extends Page:
   override def initCmd: Cmd[IO, Page.Msg] = Cmd.None
 
   override def update(msg: Page.Msg): (Page, Cmd[IO, Page.Msg]) = (this, Cmd.None)
@@ -15,4 +22,23 @@ final case class SignUpPage() extends Page:
   override def view(): Html[Page.Msg] =
     div(
       h1("Sign up Page")
+        form (
+          name    := "signup",
+          `class` := "w-full max-w-lg",
+          onEvent(
+            "submit",
+            e => {
+              e.preventDefault()
+              NoOp
+            }
+          )
+        )(
+          // inputs
+        )
     )
+
+object SignUpPage {
+  trait Msg extends Page.Msg
+
+  case object NoOp extends Msg
+}
